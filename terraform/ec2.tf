e307a5859aab935d94fc2a93c48ec35bf4646f4c
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-02-06 16:18:19 +0200

    add public ec2 instance

---
+++ terraform/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "application_instance" {
+  ami                         = var.ami
+  instance_type               = var.ec2_type
+  availability_zone           = "${var.region}a"
+  key_name                    = aws_key_pair.ssh_key_variable.key_name
+  vpc_security_group_ids      = [aws_security_group.InstanceSG.id]
+  associate_public_ip_address = true
+  
+  tags = {
+    Name = "Flask-App"
+  }
+
+
+}
