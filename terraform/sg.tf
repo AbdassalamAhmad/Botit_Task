6bfbcbbe6804b8f16aa2a4074c03202d818217b7
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-02-06 14:27:21 +0200

    add security group for ec2,allow http,ssh from all

---
+++ terraform/sg.tf
@@ -0,0 +1,29 @@
+#-------------------------- Security Group For EC2 Instance --------------------------#
+resource "aws_security_group" "InstanceSG" {
+  name = "Instance_security_group"
+
+  ingress {
+    description = "allow ssh from ALL"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow http from All"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
