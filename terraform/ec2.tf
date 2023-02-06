0b2fd55650231d00bc1beb60666c82c1b49225cf
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-02-06 16:18:40 +0200

    add user data to install docker, docker-compose

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,5 +10,14 @@ resource "aws_instance" "application_instance" {
     Name = "Flask-App"
   }
 
-
+  # Install Docker && Docker-compose
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras install docker -y
+    service docker start
+    usermod -a -G docker ec2-user
+    curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    EOF
 }
