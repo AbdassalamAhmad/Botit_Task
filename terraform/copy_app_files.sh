6faa1b05d1db02be1933d043a0cb8ef63effa157
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-02-07 10:20:02 +0200

    restructure copy files script

--- terraform/copy_app_files.sh
+++ terraform/copy_app_files.sh
@@ -1,11 +1,9 @@
+instance_ip=$(terraform output --raw instance_public_ip)
+cd ../ # go back to root folder
 # copy private ssh key from secrets into github runner.
-mkdir ../keys
-cd ../keys
-echo "$EC2_PRIVATE_SSH_KEY" > ssh_key_aws
-sudo chmod 400 ssh_key_aws
+mkdir ./keys
+echo "$EC2_PRIVATE_SSH_KEY" > ./keys/ssh_key_aws
+sudo chmod 400 ./keys/ssh_key_aws
 
 # copy files from repo to EC2 instance
-cd ../terraform
-instance_ip=$(terraform output --raw instance_public_ip)
-cd ../
 scp -o StrictHostKeyChecking=no -i ./keys/ssh_key_aws -r src/ tests/ ./requirements.txt ./docker-compose.yml ./Dockerfile ec2-user@$instance_ip:~
